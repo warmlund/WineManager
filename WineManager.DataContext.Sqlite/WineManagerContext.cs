ab4406c37c3df1d8c57633f087ee74ab24a8ee8c
warmlund <emeliewarmlund@gmail.com>
2024-12-14 12:30:45 +0100

    Setup of class library of database context using SQLite

---
+++ WineManager.DataContext.Sqlite/WineManagerContext.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WineManager.DataContext.Sqlite;
+
+namespace WineManager.EntityModels;
+
+public partial class WineManagerContext : DbContext
+{
+    public WineManagerContext()
+    {
+    }
+
+    public WineManagerContext(DbContextOptions<WineManagerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producer> Producers { get; set; }
+
+    public virtual DbSet<Wine> Wines { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            string database = "WineManager.db";
+            string dir = Environment.CurrentDirectory;
+            string path = string.Empty;
+
+            if (dir.EndsWith("net8.0"))
+            {
+                //In the <project>\bin\<Debug|Release>\net8.0\ directory.
+                path = Path.Combine("..", "..", "..", "..", database);
+            }
+
+            else
+            {
+                //In the <project> directory
+                path = Path.Combine("..", database);
+            }
+
+            path=Path.GetFullPath(path);
+            WineManagerContextLogger.WriteLine($"Database path: {path}");
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException(message: $"{path} not found.", fileName: path );
+            }
+
+            optionsBuilder.UseSqlite($"Data Source={path}");
+            optionsBuilder.LogTo(WineManagerContextLogger.WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Wine>()
+            .HasOne(w => w.Producer)
+            .WithMany(w => w.Wines)
+            .HasForeignKey(w => w.ProducerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Producer>(entity =>
+        {
+            entity.ToTable("producer");
+            entity.Property(p => p.ProducerId).HasColumnName("producerId");
+            entity.Property(p => p.ProducerName).HasColumnName("producerName");
+            entity.Property(p => p.Country).HasColumnName("country");
+            entity.Property(p => p.Region).HasColumnName("region");
+        });
+
+        modelBuilder.Entity<Wine>(entity =>
+        {
+            entity.ToTable("wine");
+            entity.Property(w => w.WineId).HasColumnName("wineId");
+            entity.Property(w => w.WineName).HasColumnName("wineName");
+            entity.Property(w => w.BottleSize).HasColumnName("bottleSize");
+            entity.Property(w => w.AlcoholContent).HasColumnName("alcoholContent");
+            entity.Property(w => w.ProducerId).HasColumnName("producerId");
+        });
+
+        modelBuilder.Entity<Wine>(entity =>
+        {
+            entity.Property(w => w.AlcoholContent)
+                .HasPrecision(3, 1);
+            entity.Property(w => w.WineName)
+                .HasMaxLength(100)
+                .IsRequired();
+        });
+
+        modelBuilder.Entity<Producer>(entity =>
+        {
+            entity.Property(p => p.ProducerName)
+                .HasMaxLength(100)
+                .IsRequired();
+            entity.Property(p => p.Region)
+                .HasMaxLength(50)
+                .IsRequired();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ WineManager.DataContext.Sqlite/WineManagerContextExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WineManager.EntityModels;
+
+namespace WineManager.DataContext.Sqlite
+{
+    public static class WineManagerContextExtensions
+    {
+        public static IServiceCollection AddWineManagerContext(this IServiceCollection services, string relativePath = "..", string databaseName = "WineManager.db")
+        {
+            string path = Path.Combine(relativePath, databaseName);
+            path = Path.GetFullPath(path);
+            WineManagerContextLogger.WriteLine($"Databse path: {path}");
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException(message: $"{path} not found.", fileName: path);
+            }
+
+            services.AddDbContext<WineManagerContext>(options =>
+            {
+                options.UseSqlite($"Data Source={path}");
+
+                options.LogTo(WineManagerContextLogger.WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+            },
+
+            contextLifetime: ServiceLifetime.Transient,
+            optionsLifetime: ServiceLifetime.Transient);
+        
+            return services;
+        }
+    }
+}
