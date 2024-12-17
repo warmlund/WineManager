5862e08ea9e0f26e94c26f7ba7bdfff0619359b8
warmlund <emeliewarmlund@gmail.com>
2024-12-17 20:46:31 +0100

    retry

--- WineManager.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WineManager.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
